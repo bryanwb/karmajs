e03adda47ec75bf4c59fb423376a6d3e0c1aea65
root <root@hitman.(none)>
2009-07-12 14:18:51 +0545

    added test.pot, first pot generated by script

--- utils/narwhal/i18n/ksetup.js
+++ utils/narwhal/i18n/ksetup.js
@@ -1,10 +1,49 @@
+// This grabs translatable strings from an html doc
 
+var srchtml = system.args[1];
+var targetpo = system.args[2];
+var myTags = 'h1, h2, h3, h4, title, button, label';
+var poArray = [];
 
+//set up the environment
 load('./lib/env.rhino.js');
-window.location = ('./i18n/test.html');
+window.location = (srchtml);
 load('./i18n/jquery.js');
-var mystr = "";
-mystr = $('meta')[0].tagName;
+
+
+//Write preamble to po file
+//This should be the first string you push
+poArray.push("Project-Id-Version: PACKAGE VERSION\n" +
+"Report-Msgid-Bugs-To: bryan@olenepal.org \n" +
+"POT-Creation-Date: \n" +
+"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n" +
+"Last-Translator: FULL NAME <EMAIL@ADDRESS>\n" +
+"Language-Team: LANGUAGE <LL@li.org>\n" +
+"MIME-Version: 1.0\n" +
+"Content-Type: text/plain; charset=CHARSET\n" +
+"Content-Transfer-Encoding: 8bit\n\n");
+
+
+$(myTags).each(function(){
+    poArray.push("msgctxt \"HTML Tag: " + $(this)[0].tagName + " ID: " + $(this).attr('id') + "\"\n");
+    poArray.push("msgid \"" + $(this).html() + "\"\n\n");
+
+});
+
+
+var printAttr = function(selector, elemAttr) {
+    $(selector).each(function(){
+	poArray.push("msgctxt \"HTML Tag: " + $(this)[0].tagName + " ID: " + $(this).attr('id') + "\"");
+	poArray.push("msgid \"" + $(this).attr(elemAttr) + "\"\n");
+
+    });
+    return this;
+};
+
+printAttr('meta', 'content');
+printAttr('img[alt]', 'alt');
+
 
 file = require('file');
-file.write('./i18n/testnw.txt', mystr); 
+file.write(targetpo, poArray.join("")); 
+
