2507e8c5e7f544f35cdc53dfcd4123b94550f58a
root <root@hitman.(none)>
2009-07-16 12:02:04 +0545

    basic image switching for locale is working, but not yet sound

--- yes_no/js/yes_no.js
+++ yes_no/js/yes_no.js
@@ -1,24 +1,28 @@
 
-    $(document).ready(function () {
+$(document).ready(function () {
+    
+    function _ (msgid) {
+	return gt.gettext(msgid);
+    }
 
-	var myLang = navigator.language;
-        myLang = "es-SP";
-
-	var params = { "domain" : "en-US",
+    var params = { "domain" : "en-US",
 	    "locale_data" : json_locale_data
 		     };
 
-	gt = new Gettext(params);
-        
-	function _ (msgid) {
-	    return gt.gettext(msgid);
-	}
+    var gt = new Gettext(params);
+       
 
+    var audio = $('audio').load();
 
+    var localize = function (newlocale) {
+
+	
 	var loadLang = function(newlocale) {
+	    //loads the .json file that holds the strings 
+	    //for the particular locale
 	    $('head').append('<script type="text/javascript"' +
-			  'src="./locale/' + newlocale +
-			  '.json"> </script>');
+			     'src="./locale/' + newlocale +
+			     '.json"> </script>');
 	    
 	    var locale_data = json_locale_data[newlocale];
 	    for (var i in locale_data){
@@ -28,46 +32,68 @@
        	    }
 	};
 
-
-
-	loadLang("es-SP");
-
-	// testing out getting strings from code
-	$('#btnLoHE').text(_("Not Hebrew"));  
-
-
-	var myscore = 0;
-	$('#btnYes').click(function () {
-	    myscore++;
-	    $("#txtScore").html("" + myscore);
-	});
-	$('#btnNo').click(function () {
-	    myscore--;
-	    $("#txtScore").html("" + myscore);
-	});
-	$('#btnRestart').click(function () {
-	    myscore=0;
-	    $("#txtScore").html("" + myscore);
-	});
- 
-	$('#btnRTL').click(function (){
-	    $('html[dir=ltr]').width() == null ? $('html').attr('dir', 'ltr') : 
-		$('html').attr('dir','rtl');
-	});
-
-	$('#btnLoEN').click(function(){ loadLang("en-US");});
-
-	$('#btnLoNE').click(function (){
+	var loadSounds = function(newlocale) {
 	    
-	});
+	};
 
-	$('#btnLoHE').click(function (){
+	var loadImages = function(newlocale) {
+	    $('img[src!=' + window.location + ']').each(function () { 
+		var imgsrc = $(this).attr("src")
+		.replace(/([^\/]*$)/, newlocale + "_$1");
+		$(this).attr('src', imgsrc);
+	    });
 	    
-	});
-
-	$('#btnLoES').click(function(){loadLang("es-SP");});
+	};
 
 
+	loadLang(newlocale);
+	loadSounds(newlocale);
+	loadImages(newlocale);
+
+    };
+
+    //loadLang("es-SP");
+
+    // testing out getting strings from code
+    $('#btnLoHE').text(_("Not Hebrew"));  
+
+
+    var myscore = 0;
+    $('#btnYes').click(function () {
+	audio[0].play()
+	myscore++;
+	$("#txtScore").html("" + myscore);
+    });
+    $('#btnNo').click(function () {
+	myscore--;
+	audio[1].play()
+	$("#txtScore").html("" + myscore);
+    });
+    $('#btnRestart').click(function () {
+	myscore=0;
+	$("#txtScore").html("" + myscore);
+    });
+    
+    $('#btnRTL').click(function (){
+	$('html[dir=ltr]').width() == null ? $('html').attr('dir', 'ltr') : 
+	    $('html').attr('dir','rtl');
+    });
+
+    $('#btnLoEN').click(function(){ 
+	localize("en-US");
+    });
+
+    $('#btnLoNE').click(function (){
 	
     });
+
+    $('#btnLoHE').click(function (){
+	localize("en-US");
+    });
+
+    $('#btnLoES').click(function(){localize("es-SP");});
+
+
+    
+});
 
