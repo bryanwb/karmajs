9171cf7daee8be3ffdf70181b30c35ed6fa64b95
christophd@sugarlabs.org <christophd@sugarlabs.org>
2009-08-06 17:08:40 +0545

    added utils/chakra.js which will be used for Chakra generation worked on E-Paath style layout

---
+++ utils/chakra.js
@@ -0,0 +1,9 @@
+// This scripts builds a Chakra based on the lesson directories
+
+// check arguments to see whether text file containing dir lists is present and which Chakra view should be greated (e-paath or list)
+
+// loop through all the dirs
+
+// if e-paath view option ->
+
+// if list view option ->
