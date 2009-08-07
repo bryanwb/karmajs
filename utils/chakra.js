2447d461f8b81b902c584d0cd700671851f59e9e
christophd@sugarlabs.org <christophd@sugarlabs.org>
2009-08-07 16:57:44 +0545

    added jquery command for listview generation, changed karma and chakra.js to UNIX compliant formating

--- utils/chakra.js
+++ utils/chakra.js
@@ -1,6 +1,36 @@
 // This scripts builds a Chakra based on the lesson directories
 
+// "Usage:  --buildchakra [OPTION] filename"
+// options: building Chakra in "e"-paath or "l"ist style, choice of grades and subjects
+// filename: contains a list of the lesson directories
+
+//only include certain grade and subject combinations
+
+var option = "";
+var directorylist = "";
+var chakraoutput = "";
+
+
+
+// testing
+
+// For Chakra grade / subject menu
+//$('<li id="grade7"><a href="#tab_grade7">Grade 7</a></li>').insertBefore('#menu_assessment');
+//$('<div id="tab_grade7"><div id="grade7">Grade 7</div></div>').insertBefore('#tab_assessment');
+//$('<ul id="grade-menu" class="usual"></ul>').prependTo('#tab_grade7');
+//$('<li id="grade7e"><a href="#tab_grade7e">English</a></li>').prependTo('#tab_grade7 ul');
+//$('<div id="tab_grade7e"><div id="grade7e">Grade 7 English</div></div>').insertBefore('#tab_assessment');
+
+// For Chakra list view
+//$('<div id="lesson_item"><div id="name"><a href=""></a><img src="testdata/preview.jpg"/></div><div id="grade_week"></div><div id="docs"><a href="">Teachers Notes</a> <a href="">Lesson Plan</a></div></div><div id="lesson_separator" class="lesson_separator"></div>').insertAfter('#lesson_item');
+
+// /testing
+
+print('Hello world');
+
 // check arguments to see whether text file containing dir lists is present and which Chakra view should be greated (e-paath or list)
+
+
 
 // loop through all the dirs
 
