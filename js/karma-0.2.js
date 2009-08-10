3a05ae90cfbf80dd0535af33d6df76b64bff643c
Felipe Lopez Toledo <zer.subzero@gmail.com>
2009-08-10 17:40:34 -0500

    Karma Plugin: 0.2-dev

---
+++ js/karma-0.2.js
@@ -0,0 +1,406 @@
+/**
+* Karma Framework
+* http://wiki.sugarlabs.org/go/Karma
+* under MIT license: http://dev.jquery.com/browser/trunk/jquery/MIT-LICENSE.txt
+*
+*
+*/
+//
+
+
+
+var Karma = function( options ) {
+	var that = this;
+	this.version = "0.2 alpha";
+	//
+	//relative path to the po, images, sounds, etc.  from the html
+	//defined here: http://wiki.sugarlabs.org/go/Karma/Bundle_layout
+	//localized is recalculated inside localizeContent ( $ = language.lang )
+	this.paths = {
+		po: "po/",
+		images: {
+				localized:	"assets/$/images/",
+				generic:	"assets/generic/images/"
+				},
+		sounds: {
+				localized:	"assets/$/sounds/",
+				generic:	"assets/generic/sounds/"
+				},
+		videos: {
+				localized:	"assets/$/videos/",
+				generic:	"assets/generic/videos/"
+				}
+	};
+	this.supportedLangFiles = [ 
+		{ ext: "po",   type: 'application/x-po' },
+		{ ext: "json", type: 'application/json'}
+	];
+	//
+	//PRIVATE STUFF start
+	/**
+	* getLanguage
+	* get the language acording to the browser language
+	*/
+	var getLanguage = function () {
+		//console.log +=  navigator.language +"\n";
+		var lang = navigator.language || navigator.browserLanguage; //mozilla / ie
+		lang = lang.replace(/_/, '-').toLowerCase();
+		if (lang.length > 3 ) {
+			var country = lang.substring(3, 5);
+			lang = lang.substring(0, 2);
+			if ( country.match(/[^a-zA-Z]/) === null ) {
+				country = country.toUpperCase();
+				return  { "lang": lang + "-" + country, "langCode": lang, "countryCode": country };
+			}
+		}
+		return { "lang": lang };
+	}
+	/**
+	*i18nWrapper
+	*creates a new Gettext object and returns a shortcut function to translate strings
+	*/
+	var i18nWrapper = function ( options ) {
+		var gt = new Gettext( options );
+		if ( typeof ( gt ) === 'undefined' )
+			throw new Error("Unable to initialize Gettext object");
+		return (function (str1, str2, str3, str4 ) {
+			var n, context, singular, plural;
+			if (typeof(str4) != 'undefined') {
+				// number, context, singular, plural
+				return gt.npgettext(str2, str3, str4, str1);
+			} else if (typeof(str3) != 'undefined') {
+				// number, singular, plural
+				return gt.ngettext(str2, str3, str1);
+			} else if (typeof(str2) != 'undefined') {
+				// context, msgid
+				return gt.pgettext(str1, str2);
+			} else if (typeof(str1) != 'undefined') {
+				// msgid
+				return gt.gettext(str1);
+			} else {
+				// nothing passed in; return blank string.
+				// XXX: we could error here, but that may cause more harm than good.
+				return '';
+			}
+		});
+	};
+	/**
+	*
+	*/
+	var localiseContent = function ( lang ) {
+		
+		var toFix = ["images", "sounds", "videos"];
+		for (var i = 0; i < toFix.length; i++) {
+			that.paths[ toFix[ i ] ].localized = that.paths[ toFix[ i ] ].localized.replace('\$', lang );
+		}
+	}
+	/**
+	*
+	*/
+	var loadAlternatives = function ( ) {
+		var loaded = undefined;
+		var tryNext = true;
+		//try to load the po or json language file if it exists. 
+		//the lang order is acording to options.language.alternatives
+		//the type (po or json or ...) is defined in supportedLangFiles
+		$.each( that.language.alternatives, function ( c, lang ) {
+			for (var i=0; i < that.supportedLangFiles.length && tryNext === true; i++) {
+				$.ajax({
+					url: that.paths.po + lang + "." + that.supportedLangFiles[i].ext,
+					cache: true,
+					dataType: "text",
+					async: false, //important: touch it at your own risk
+					success: function( data, textStatus ){
+						
+						loaded =  lang + "." + that.supportedLangFiles[i].ext;
+						//i18n
+						//we pass the data so we avoid re-loading the file
+						//creates the shorcout
+						that.i18n.root[ that.i18n.shortcut ] =  i18nWrapper(
+							{ 
+								domain 	: lang, 
+								file 	: { 
+											type: that.supportedLangFiles[i].type , 
+											uri: this.url, data: data 
+										} 
+							}
+						);
+						localiseContent( lang );
+						tryNext = false;
+					},
+					error: function ( XHR, textStatus, errorThrown ) {
+						//the file doesn't exist or it wasn't possible to load it
+						tryNext = true;
+					}
+				});
+				return tryNext;
+			}
+		});
+		return loaded;
+	}
+	//PRIVATE STUFF end
+	// default options 
+	var defaultOptions ={
+		container:   "#karma-main",
+		language:   { 
+						lang: 			undefined,
+						alternatives: 	['en-US', 'en'],
+						countryCode: 	undefined,
+						langCode:		undefined,
+					},
+		i18n:		{
+						root: 			self, // self is global
+						shortcut: 		"_"
+					},
+		canvas:  	undefined,
+		width:      100,
+		height:     100,
+		fps:		24
+	};
+	//
+	this.output={ 
+		language: 	{	 },
+		library:	{ images: [], sounds: [], videos:[], shapes:[] }
+	};
+	
+	//initializes the defaultOptions argument
+	//1 argument: string.  assume it's the container
+	if ( typeof options === "string" ) {
+		options = { container: options };
+		options.language = getLanguage() ;
+	} else if (typeof options === "object" ){
+		if ( typeof options.lang === "string" ) {
+			//if language is string, assume  it's the language.lang
+			options.language = { lang: options.lang };
+		}
+	}
+	$.extend( true, defaultOptions, options );
+	//
+	//copy defaultOptions to this, we use this.xyz instead this.defaultOptions.xyz 
+	for (var i in defaultOptions ) {
+		this[ i ] = defaultOptions[i];
+	}
+	
+	//initializes i18n
+	//add the localized language to the language.alternatives
+	if ( typeof this.language.countryCode !== "undefined" ) {
+		this.language.alternatives.unshift( this.language.langCode, this.language.countryCode );
+	}
+	if ( typeof this.language.lang !== "undefined" ) {
+		this.language.alternatives.unshift( this.language.lang );
+	}
+	//try to load the localized lang file (po or json or ...)
+	this.output.language.loaded = loadAlternatives( );
+	//
+
+	//initializes the container
+	if ( typeof this.container === "string" ) {
+		this.container = $( this.container );
+	}
+	//
+	gk = {
+		"paths" : this.paths,
+		"container" : this.container
+	}
+}
+
+/**
+*
+**/
+Karma.prototype.size = function ( w, h) {
+	this.canvas = document.createElement("canvas");
+	this.canvas.width  = this.width  = ( this.width  || w );
+	this.canvas.height = this.height = ( this.height || h );
+	if ( this.canvas.getContext ) {
+		this.ctx = this.canvas.getContext("2d");
+		this.container[ 0 ].appendChild( this.canvas );
+	}else {
+		throw new Error ("Your browser doesn't support canvas, try Firefox or Google chrome");
+	}
+	gk.canvas = this.canvas;
+	return this;
+}
+
+Karma.prototype.geometry = {
+	radians : function( angle ){
+		return ( angle / 180 ) * Math.PI;
+	}
+}
+
+/*
+* Master Class creator
+*supports multiple inheritance, warning it's NOT optimal
+*/
+var Class = function () {
+	var o = function ( options ) {
+		if( this.init )
+			this.init.apply( this, options );
+	};
+	o.prototype ={};
+	var a;
+	var s="";
+	for ( var i =0; i < arguments.length; i++) {
+		a = arguments[i];
+		s += typeof a+"\n";
+		if ( typeof a === "function") {
+			a = a();
+		}
+		if ( typeof a === "object") {
+			for (var j in a) {
+				s += j+" = "+a[j]+"\n";
+				o[ j ] = o.prototype[ j ] = a [ j ];
+			}
+		}
+		
+	}
+	//alert(s);
+	return (function ( ) { return new o( arguments );});
+}
+
+
+var kObject = Class(
+	{
+		init: function ( options ) {
+			if (options && typeof options.localized === "boolean" ) {//FIXME
+				this.localized = options.localized;
+			}else {
+				this.localized = true;
+			}
+		}
+	}
+);
+var graphic = Class(
+	kObject,
+	{
+		init: function ( options ) {
+			var defaultOptions = {
+				x : 0,
+				y : 0,
+				parent : undefined,
+				visible : true
+			}
+			$.extend( this, defaultOptions, options);
+			if ( options !== "undefined" )
+				kObject().init.call (this, options);
+		},
+		isPointInPath : function() {},
+		draw : function() {}
+	}
+);
+
+var media = Class(
+	kObject,
+	{
+		init: function ( options ) {
+			this.file = undefined;
+			this._type = undefined;
+			this._status = undefined;
+			this.src = undefined;
+			this.media = undefined;
+			this.fullPath = undefined;
+			if ( options !== "undefined" )
+				kObject().init.call (this, options);
+		},
+		localize: function () {
+			
+		}
+	}
+);
+
+var image = Class(
+	graphic,
+	media,
+	{
+		init: function ( options ) {
+			var defaultOptions = {
+				w : undefined,
+				h : undefined,
+			}
+			$.extend( this, defaultOptions, options);
+
+			if ( options !== "undefined" )
+				media().init.call(this, options );
+			alert (this.localized);
+		},
+		isPointInPath : function() {},
+		draw : function() {
+			
+		}
+	}
+);
+
+/*
+var KObject = function () {
+	this.localized = true; //default true: all the content will be localized 
+}
+
+var KGraphic = function () {
+	KObject.apply (this, arguments);
+	var that = this;
+	this.parent = undefined,
+	this.childNodes = [];
+	this.visible = true;
+	this.mouse = "default";
+	this.gx=0;
+	this.gy=0;
+	this.x =0;
+	this.y =0;
+}
+KGraphic.prototype = new KObject();
+KGraphic.prototype.isPointInPath = function() { }
+KGraphic.prototype.draw = function() { }
+
+
+var KMedia = function () {
+	KObject.apply (this, arguments);
+	var that = this;
+	this._type = undefined;
+	this._status = undefined;
+	this.src = undefined;
+	this.media = undefined;
+	if ( typeof this.media !== "undefined" ){
+		this.media.addEventListener( "onload",  function (e) { that._status = "loaded"; } )
+		this.media.addEventListener( "onerror", function (e) { that._status = "error"; } )
+		this.media.addEventListener( "onabort", function (e) { that._status = "aborted"; } )
+	}
+}
+KMedia.prototype = new KObject();
+KMedia.prototype.f = function () {alert("f");}
+KGraphic.prototype.g = function () {alert("g");}
+
+var KImage = function ( options ) {
+	KGraphic.apply (this, arguments);
+	//KMedia.apply (this, arguments);
+	this.width = 0;
+	this.height = 0;
+	
+	
+}
+KImage.prototype = new KGraphic();
+//KImage.prototype = new KMedia();
+
+
+
+var t = new KImage ();
+*/
+
+
+var KClip = function ( options ) {
+	var defaultOptions = {
+		shapes: []
+	};
+	$.extend( defaultOptions, options );
+	//copy defaultOptions to this
+	for (var i in defaultOptions ) {
+		this[ i ] = defaultOptions[ i ];
+	}
+	
+}
+
+//
+//karma wrapper, we avoid using "new"
+karma = function (options) {
+	var k =new Karma( options );
+	var img1 = image({file: "ok.png", localized: true});
+	return k;
+}
