1852015e2577acfffdc809d8e03783c86e601d2b
Bryan Berry <bryan@olenepal.org>
2010-02-02 18:28:43 +0545

    use kDoc.html to hold contain original html for lessonPlan and teachersNote in iframe

--- js/kDoc.js
+++ js/kDoc.js
@@ -1,60 +1,71 @@
-$(document).ready(function(){
+$(document).ready(
+    function(){
 
-var $help = $('#kHelpText').dialog({
-				      position:[ "right", "top"], 
-				      modal:'true',autoOpen:false,width:500,
-				      height: 400
-				  });
-
-$('#kHeaderHelpBtn').click(function(){ $help.dialog('open');});
-
-    
-$('#kHeaderBackBtn').click(function(){
-	console.log('foobar');
 	var back = 'index.html';
-	var backHash = window.location.hash;
-	if (backHash){
-	    back = backHash.split('#')[1];
-	}
+
+
+	var $help = $('#kHelpText').dialog({
+		position:[ "right", "top"], 
+		modal:'true',autoOpen:false,width:500,
+		height: 400
+		});
+
+	var parseUrlParams = function(){
+	    
+	    var doc = '';
+	    var params = window.location.search.slice(1).split('&');
+	    if (params){
+		back = params[0].split('=')[1];
+		doc = params[1].split('=')[1];
+	    }
+
+	    $('#iframeLessonPlan').attr('src', "" + doc + ".html");
+	};
+
+	parseUrlParams();
+		
+	$('#kHeaderHelpBtn').click(function(){ $help.dialog('open');});
+
 	
-	window.location = back;
-    }
-);
+	$('#kHeaderBackBtn').click(function(){
+				       window.location = back;
+				   });
 
-$('#iframeLessonPlan').load(function(){
+	$('#iframeLessonPlan').load(
+	    function(){
+		
+		var iframeScale = 1.0;
+		var iframeStyle = window.frames[0].document.body.style;
+		var translateY = 0;
+
+		$('#zoomIn').click(
+		    function(){
+			iframeScale = iframeScale + 0.1;
+			translateY = translateY + 50;
+			var scale = 'scale(' + iframeScale + ')';
+			var translate = "translate(0px, " + translateY + "px)";
+
+			iframeStyle.MozTransform = scale + ' ' + translate;
+			iframeStyle.WebkitTransform = scale + ' ' + translate;
+ 		    });
+		
+		$('#zoomOut').click(
+		    function(){
+			iframeScale = iframeScale - 0.1;
+			translateY = translateY - 50;
+			
+			var scale = 'scale(' + iframeScale + ')';
+			var translate = "translate(0px, " + translateY + "px)";
+
+
+			iframeStyle.MozTransform = scale + ' ' + translate;
+			iframeStyle.WebkitTransform = scale + ' ' + translate;
+
+		    });
+		
+	    });
+
+	Karma.scaleToViewport();
 	
-	 var iframeScale = 1.0;
-	 var iframeStyle = window.frames[0].document.body.style;
-	 var translateY = 0;
-
-	 $('#zoomIn').click(
-	     function(){
-		 iframeScale = iframeScale + 0.1;
-		 translateY = translateY + 50;
-		 var scale = 'scale(' + iframeScale + ')';
-		 var translate = "translate(0px, " + translateY + "px)";
-
-		 iframeStyle.MozTransform = scale + ' ' + translate;
-		 iframeStyle.WebkitTransform = scale + ' ' + translate;
- 	     });
-				 
-	 $('#zoomOut').click(
-		 function(){
-		 iframeScale = iframeScale - 0.1;
-		 translateY = translateY - 50;
-		 
-		 var scale = 'scale(' + iframeScale + ')';
-		     var translate = "translate(0px, " + translateY + "px)";
-
-
-		 iframeStyle.MozTransform = scale + ' ' + translate;
-		 iframeStyle.WebkitTransform = scale + ' ' + translate;
-
-		 });
-    
-	});
-
-Karma.scaleToViewport();
-		      
 
 });
